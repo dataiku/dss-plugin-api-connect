c2bcfe6f40a1546a2efba7ec0c43d939e146e9e4
Alexandre Bourret <alexandre.bourret@dataiku.com>
2020-10-22 14:30:27 +0200

    Add OAuth 2 token + auths renaming

--- python-lib/rest_api_client.py
+++ python-lib/rest_api_client.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import re
 from pagination import Pagination
 from safe_logger import SafeLogger
 from loop_detector import LoopDetector
@@ -12,7 +13,7 @@ class RestAPIClient(object):
 
     def __init__(self, credential, endpoint):
         logger.info("Initialising RestAPIClient, credential={}, endpoint={}".format(logger.filter_secrets(credential), endpoint))
-        
+
         #  presets_variables contains all variables available in templates using the {{variable_name}} notation
         self.presets_variables = {}
         self.presets_variables.update(endpoint)
@@ -27,11 +28,10 @@ class RestAPIClient(object):
         self.presets_variables.update(self.user_defined_keys)
 
         endpoint_url = endpoint.get("endpoint_url", "")
-        self.endpoint_url = self.format_postman_template(endpoint_url, **self.presets_variables)
+        self.endpoint_url = format_postman_template(endpoint_url, **self.presets_variables)
 
         endpoint_headers = endpoint.get("endpoint_headers", "")
         self.endpoint_headers = self.get_params(endpoint_headers, self.presets_variables)
-        self.requests_kwargs.update({"headers": self.endpoint_headers})
 
         login_type = credential.get("login_type", "no_auth")
         self.extraction_key = endpoint.get("extraction_key", None)
@@ -42,6 +42,12 @@ class RestAPIClient(object):
             self.requests_kwargs.update({"auth": self.auth})
         if login_type == "token":
             self.token = credential.get("token", "")
+        if login_type == "oauth_2_token":
+            self.token = credential.get("token", "")
+            bearer_template = credential.get("bearer_template", "Bearer {{token}}")
+            self.endpoint_headers.update({"Authentication": bearer_template})
+
+        self.requests_kwargs.update({"headers": self.endpoint_headers})
         self.params = self.get_params(self.endpoint_query_string, self.presets_variables)
         self.ignore_ssl_check = endpoint.get("ignore_ssl_check", False)
         if self.ignore_ssl_check:
@@ -79,6 +85,7 @@ class RestAPIClient(object):
         self.enforce_throttling()
         if self.loop_detector.is_stuck_in_loop(url, kwargs.get("params", {}), kwargs.get("headers", {})):
             raise DataikuException("We are stuck in a loop. Check pagination parameters.")
+        kwargs = template_dict(kwargs, **self.presets_variables)
         response = requests.get(url, **kwargs)
         self.time_last_request = time.time()
         if response.status_code >= 400:
@@ -104,7 +111,7 @@ class RestAPIClient(object):
         for key_value in endpoint_query_string:
             key = key_value.get("from")
             value = key_value.get("to")
-            ret.update({key: self.format_postman_template(value, **keywords)})
+            ret.update({key: format_postman_template(value, **keywords)})
         return ret
 
     def format_template(self, template, **kwargs):
@@ -112,12 +119,6 @@ class RestAPIClient(object):
             template = template.format(**kwargs)
         except KeyError as key:  # This has to go
             logger.error('Key {} not found for template "{}"'.format(key, template))
-        return template
-
-    def format_postman_template(self, template, **kwargs):
-        for key in kwargs:
-            token = "{{" + str(key) + "}}"
-            template = template.replace(token, str(kwargs.get(key)))
         return template
 
     def has_more_data(self):
@@ -133,3 +134,23 @@ class RestAPIClient(object):
             if time_since_last_resquests < self.time_between_requests:
                 logger.info("Enforcing {}s throttling".format(self.time_between_requests - time_since_last_resquests))
                 time.sleep(self.time_between_requests - time_since_last_resquests)
+
+
+def template_dict(dictionnary, **kwargs):
+    ret = dict.copy(dictionnary)
+    for key in ret:
+        if isinstance(ret[key], dict):
+            ret[key] = template_dict(ret[key], **kwargs)
+        if isinstance(ret[key], str):
+            ret[key] = format_postman_template(ret[key], **kwargs)
+            return ret
+    return ret
+
+
+def format_postman_template(template, **kwargs):
+    placeholders = re.findall(r'{([a-zA-Z\-\_]*)}', template)
+    formated = template
+    for placeholder in placeholders:
+        replacement = kwargs.get(placeholder, "")
+        formated = formated.replace("{{{{{}}}}}".format(placeholder), str(replacement))
+    return formated
