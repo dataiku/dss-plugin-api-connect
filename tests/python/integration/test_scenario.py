46d58da8d17a86b737b6bccc8d55e628f035dc28
Alexandre Bourret <alexandre.bourret@dataiku.com>
2021-10-20 13:56:20 +0200

    Add integration tests for json output, rfc5988, auth, recipe

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -9,3 +9,11 @@ def test_run_api_connect_authentication_modes(user_dss_clients):
 
 def test_run_api_connect_pagination_modes(user_dss_clients):
     dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="PAGINATION")
+
+
+def test_run_api_connect_recipes(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="Recipes")
+
+
+def test_run_api_connect_using_global_variable(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="UsingGlobalVariable")
