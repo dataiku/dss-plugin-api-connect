79f8eb86774aca303b756a89f1bb22edfab4cc07
Alexandre Bourret <alexandre.bourret@dataiku.com>
2021-11-12 17:08:28 +0100

    Add integration test for path search

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -21,3 +21,7 @@ def test_run_api_connect_using_global_variable(user_dss_clients):
 
 def test_run_api_connect_array_api(user_dss_clients):
     dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="ArrayAPI")
+
+
+def test_run_api_connect_search_path(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="SearchPath")
