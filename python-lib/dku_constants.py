6e820897796a1cc2279ca30b58b839649e5582fa
Alexandre Bourret <alexandre.bourret@dataiku.com>
2023-02-28 10:20:07 +0100

    reordering

--- python-lib/dku_constants.py
+++ python-lib/dku_constants.py
@@ -1,6 +1,6 @@
 class DKUConstants(object):
     API_RESPONSE_KEY = "api_response"
-    RAW_BODY_FORMAT = "RAW"
+    FORBIDDEN_KEYS = ["token", "password", "api_key_value"]
     FORM_DATA_BODY_FORMAT = "FORM_DATA"
     PLUGIN_VERSION = "1.1.3-beta.1"
-    FORBIDDEN_KEYS = ["token", "password", "api_key_value"]
+    RAW_BODY_FORMAT = "RAW"
