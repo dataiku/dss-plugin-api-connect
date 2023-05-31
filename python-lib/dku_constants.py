c57ea5ac651dd2318cb725d3ff95c7ed11ba5432
Alexandre Bourret <alexandre.bourret@dataiku.com>
2023-05-31 12:49:16 +0200

    version update

--- python-lib/dku_constants.py
+++ python-lib/dku_constants.py
@@ -2,5 +2,5 @@ class DKUConstants(object):
     API_RESPONSE_KEY = "api_response"
     FORBIDDEN_KEYS = ["token", "password", "api_key_value"]
     FORM_DATA_BODY_FORMAT = "FORM_DATA"
-    PLUGIN_VERSION = "1.1.3-beta.1"
+    PLUGIN_VERSION = "1.2.0"
     RAW_BODY_FORMAT = "RAW"
