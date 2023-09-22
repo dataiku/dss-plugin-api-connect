99b301414709a97160eb9293858beba0d1676b8f
Guillaume Redoulès <guillaume.redoules@gadz.org>
2023-09-22 11:16:27 +0200

    Add files via upload

--- custom-recipes/api-connect/recipe.py
+++ custom-recipes/api-connect/recipe.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 import dataiku
-from dataiku.customrecipe import get_input_names_for_role, get_recipe_config, get_output_names_for_role
+from dataiku.customrecipe import (
+    get_input_names_for_role,
+    get_recipe_config,
+    get_output_names_for_role,
+)
 import pandas as pd
 from safe_logger import SafeLogger
 from dku_utils import get_dku_key_values, get_endpoint_parameters
 from rest_api_recipe_session import RestApiRecipeSession
 
+import os
 
 logger = SafeLogger("api-connect plugin", forbiden_keys=["token", "password"])
 
@@ -25,15 +30,16 @@ def get_partitioning_keys(id_list, dku_flow_variables):
     return partitioning_keys
 
 
-logger.info('API-Connect plugin recipe v1.1.3')
+logger.info("API-Connect plugin recipe v1.1.3")
 
-input_A_names = get_input_names_for_role('input_A_role')
+input_A_names = get_input_names_for_role("input_A_role")
 config = get_recipe_config()
 dku_flow_variables = dataiku.get_flow_variables()
 
 logger.info("config={}".format(logger.filter_secrets(config)))
 
 credential_parameters = config.get("credential", {})
+noproxy_parameters = config.get("no_proxy", {})
 endpoint_parameters = get_endpoint_parameters(config)
 extraction_key = endpoint_parameters.get("extraction_key", "")
 is_raw_output = endpoint_parameters.get("raw_output", True)
@@ -52,16 +58,17 @@ input_parameters_dataframe = input_parameters_dataset.get_dataframe()
 recipe_session = RestApiRecipeSession(
     custom_key_values,
     credential_parameters,
+    noproxy_parameters,
     endpoint_parameters,
     extraction_key,
     parameter_columns,
     parameter_renamings,
     display_metadata,
-    maximum_number_rows=maximum_number_rows
+    maximum_number_rows=maximum_number_rows,
 )
 results = recipe_session.process_dataframe(input_parameters_dataframe, is_raw_output)
 
-output_names_stats = get_output_names_for_role('api_output')
+output_names_stats = get_output_names_for_role("api_output")
 odf = pd.DataFrame(results)
 
 if odf.size > 0:
