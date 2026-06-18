fad7f985d0ee3de57aa90e664ace39b32d403073
Alex Bourret <alexandre.bourret@dataiku.com>
2026-06-18 13:55:35 +0200

    removing beta marker

--- python-lib/dku_constants.py
+++ python-lib/dku_constants.py
@@ -2,7 +2,7 @@ class DKUConstants(object):
     API_RESPONSE_KEY = "api_response"
     FORBIDDEN_KEYS = ["token", "password", "api_key_value", "secure_token", "mtls_key_path", "mtls_certificate_path"]
     FORM_DATA_BODY_FORMAT = "FORM_DATA"
-    PLUGIN_VERSION = "1.4.0-beta.4"
+    PLUGIN_VERSION = "1.4.0"
     RAW_BODY_FORMAT = "RAW"
     MULTIPART_FORM_DATA_BODY_FORMAT = "MULTIPART_FORM_DATA"
     REPONSE_ERROR_KEY = "dku_error"
