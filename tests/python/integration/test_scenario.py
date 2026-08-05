1dd9855dfe0a2c53c9e60fd0c081c36a93d3a56f
Alex Bourret <alexandre.bourret@dataiku.com>
2026-08-05 13:57:15 +0700

    update changelog

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -69,3 +69,7 @@ def test_run_api_connect_multipart_form_data(user_dss_clients):
 
 def test_run_api_connect_multipart_form_data(user_dss_clients):
     dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="MULTIPARTFORMDATATEMPLATING")
+
+
+def test_run_api_connect_multipart_form_data(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="NULLININPUTDATASET")
