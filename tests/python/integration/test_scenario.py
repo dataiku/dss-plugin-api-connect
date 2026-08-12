93df350b774dd6c3cce6f0c1f6b74e3ad7ca4d6c
TRAN Duy Nghia <tdnghia98@gmail.com>
2026-08-12 10:22:41 +0200

    Restore multipart integration scenario test

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -67,5 +67,5 @@ def test_run_api_connect_multipart_form_data(user_dss_clients):
     dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="MULTIPARTFORMDATA")
 
 
-def test_run_api_connect_multipart_form_data(user_dss_clients):
+def test_run_api_connect_multipart_form_data_templating(user_dss_clients):
     dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="MULTIPARTFORMDATATEMPLATING")
